2aad210381771957de39ffa262409de13ba3bb7c
Tamer Mohamed <kalila.tamer@gmail.com>
2026-05-02 10:40:20 +0200

    fixed a bug

--- property_management/overrides/payment_entry.py
+++ property_management/overrides/payment_entry.py
@@ -2,27 +2,34 @@ import frappe
 
 
 def validate(doc, method):
-    lease_contract = getattr(doc, 'custom_lease_contract', None)
 
-    if not lease_contract:
+
+    if doc.payment_type != "Receive" or doc.party_type != "Customer":
         return
 
-    monthly_rent = frappe.db.get_value(
+    lease = frappe.db.get_value(
         "Lease Contract",
-        lease_contract,
-        "monthly_rent"
+        {
+            "tenant": doc.party,
+            "docstatus": 1
+        },
+        ["name", "monthly_rent"],
+        as_dict=True
     )
 
-    if not monthly_rent:
+    if not lease:
+        return
+
+    monthly_rent = lease.monthly_rent
+
+    if doc.paid_amount < monthly_rent:
         frappe.throw(
-            f"Could not find monthly rent for Lease Contract "
-            f"{lease_contract}. "
-            "Please make sure the lease exists and is submitted."
+            f"Payment amount ({doc.paid_amount}) is less than "
+            f"the monthly rent ({monthly_rent})."
         )
 
-    if doc.paid_amount < monthly_rent or doc.paid_amount > monthly_rent:
+    if doc.paid_amount > monthly_rent:
         frappe.throw(
-            f"Payment amount ({doc.paid_amount}) cannot be less OR Bigger "
-            f"than the monthly rent ({monthly_rent}) "
-            f"for Lease Contract {lease_contract}."
+            f"Payment amount ({doc.paid_amount}) is greater than "
+            f"the monthly rent ({monthly_rent})."
         )
